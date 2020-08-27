556b70b8888c22bd74aabf408d231987983f1b07
jachs <799516423@qq.com>
2020-08-27 09:39:46 +0800

    first commit

---
+++ spring-el/src/test/java/com/jachs/springel/SpringElApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jachs.springel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringElApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
