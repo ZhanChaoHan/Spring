ce66df7715278ce961ce9e4d60647e2fea304dd0
“我听别人说，这世界上有一种电脑是没有关机键的，它只能够一直的运行呀运行呀，运行累了就在数据流里面睡一会，这种电脑一辈子只能关机一次，那一次就是它宕机的时候。” ——《阿机正传》 <799516423@qq.com>
2020-12-19 11:31:14 +0800

    Add files via upload

---
+++ spring-boot-jpa/src/main/java/com/jachs/jpa/dao/oto/StudentCardRepository.java
@@ -0,0 +1,13 @@
+package com.jachs.jpa.dao.oto;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jachs.jpa.entity.oto.StudentCard;
+
+/**
+ * @author zhanchaohan
+ * 
+ */
+public interface StudentCardRepository extends JpaRepository<StudentCard,String>{
+
+}
