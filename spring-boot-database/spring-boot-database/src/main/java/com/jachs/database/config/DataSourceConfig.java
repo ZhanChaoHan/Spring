1c68ee3958cab8a334cc32cd2470e46d016bd1d5
jachs <799516423@qq.com>
2020-12-24 13:13:15 +0800

    QWQC

--- spring-boot-database/spring-boot-database/src/main/java/com/jachs/database/config/DataSourceConfig.java
+++ spring-boot-database/spring-boot-database/src/main/java/com/jachs/database/config/DataSourceConfig.java
@@ -1,8 +1,10 @@
 package com.jachs.database.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import com.alibaba.druid.support.http.WebStatFilter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -10,27 +12,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Configuration
-@PropertySource(value = "classpath:/druid.properties")
-@ConfigurationProperties( "spring.datasource.druid" )
+@PropertySource(value = "classpath:druid.properties")
 public class DataSourceConfig {
     /**
          * 虽然我们配置了druid连接池的其它属性，但是不会生效。因为默认是使用的java.sql.Datasource的类来获取属性的，
          * 有些属性datasource没有。如果我们想让配置生效，需要手动创建Druid的配置文件
+     * @throws IOException 
      */ 
-    @Bean
-    public DataSource dataSource () {
+    /* @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource () throws IOException {
+        Properties pro=new Properties();
+        pro.load ( DataSourceConfig.class.getResourceAsStream ( "/druid2.properties" ) );
         DruidDataSource dataSource = new DruidDataSource ();
-        //请求失败后是否中断
-        dataSource.setBreakAfterAcquireFailure ( true );
-        //重试次数
-        dataSource.setConnectionErrorRetryAttempts ( 0 );
+        dataSource.setConnectProperties ( pro );
         return dataSource;
-    }
+    //        return  DruidDataSourceBuilder.create ().build ();
+    }*/
 
     /**
      * Druid的最强大之处在于它有着强大的监控，可以监控我们发送到数据库的所有sql语句。方便我们后期排插错误 配置Druid的监控
