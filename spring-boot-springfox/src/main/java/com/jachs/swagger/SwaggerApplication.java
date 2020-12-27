8e1471b9be8a2c2e31343e2b64d9fd39ba228040
799516423@qq.com <github_jachs22>
2020-12-27 16:17:04 +0800

    'acas

---
+++ spring-boot-springfox/src/main/java/com/jachs/swagger/SwaggerApplication.java
@@ -0,0 +1,17 @@
+package com.jachs.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/****
+ * 访问地址:http://localhost:8080/swagger-ui.html
+ * @author zhanchaohan
+ *
+ */
+@SpringBootApplication
+public class SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApplication.class, args);
+	}
+}
+
