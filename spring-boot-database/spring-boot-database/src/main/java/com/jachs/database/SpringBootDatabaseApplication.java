112e648af7cb8c56832261df6cceaa555370a58c
799516423@qq.com <github_jachs22>
2020-12-27 18:16:01 +0800

    qsaxc

--- spring-boot-database/spring-boot-database/src/main/java/com/jachs/database/SpringBootDatabaseApplication.java
+++ spring-boot-database/spring-boot-database/src/main/java/com/jachs/database/SpringBootDatabaseApplication.java
@@ -3,9 +3,9 @@ package com.jachs.database;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootDatabaseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDatabaseApplication.class, args);
 	}
--- spring-boot-database/spring-boot-database/src/test/java/com/jachs/database/TestDefaultSource.java
+++ spring-boot-database/spring-boot-database/src/test/java/com/jachs/database/TestDefaultSource.java
@@ -9,6 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.jachs.database.config.properties.C3p0Properties;
+import com.jachs.database.config.properties.DbcpProperties;
+
+
 /***
  * Springboot默认支持4种数据源类型，定义在 org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
  * 中，分别是： <br>
@@ -26,6 +30,11 @@ public class TestDefaultSource {
     @Autowired
     private DataSourceProperties dataSourceProperties;
 
+    @Autowired
+	private C3p0Properties c3p0Properties;
+	@Autowired
+	private DbcpProperties dbcpProperties;
+	
     @Test
     public void testDs () throws SQLException {
         System.out.println ( "用户名：" + dataSourceProperties.getUsername () );
@@ -36,7 +45,6 @@ public class TestDefaultSource {
         
         System.out.println ( "得到的数据源：" + ds );
         System.out.println ( "得到的连接：" + ds.getConnection () );
-        
         
     }
 }
