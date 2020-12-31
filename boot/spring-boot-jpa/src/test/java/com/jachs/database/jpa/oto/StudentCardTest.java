b45e0c21d443212ae479975862aac18fd6b1b490
jachs <799516423@qq.com>
2020-12-31 14:12:05 +0800

    ggggggg

---
+++ boot/spring-boot-jpa/src/test/java/com/jachs/database/jpa/oto/StudentCardTest.java
@@ -0,0 +1,27 @@
+package com.jachs.database.jpa.oto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.jachs.database.jpa.dao.oto.StudentCardRepository;
+import com.jachs.database.jpa.entity.oto.StudentCard;
+
+/**
+ * @author zhanchaohan
+ * 
+ */
+@SpringBootTest
+public class StudentCardTest {
+    @Autowired
+    private StudentCardRepository studentCardRepository;
+    
+    @Test
+    public void testAddOne() {
+        StudentCard sc=new StudentCard();
+        sc.setCardId ( "cId" );
+        sc.setCardMonery ( 598475L );
+        sc.setCardType ( "学生卡" );
+        studentCardRepository.save ( sc );
+    }
+}
