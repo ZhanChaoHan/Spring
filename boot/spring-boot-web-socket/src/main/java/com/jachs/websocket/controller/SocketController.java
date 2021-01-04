6f39f740a5755acd0f43bbc0b1a6bf7af2a59e16
jachs <799516423@qq.com>
2021-01-04 11:22:15 +0800

    'adce'

---
+++ boot/spring-boot-web-socket/src/main/java/com/jachs/websocket/controller/SocketController.java
@@ -0,0 +1,39 @@
+package com.jachs.websocket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jachs.websocket.servlet.WebSocketServer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhanchaohan
+ * 
+ */
+@Controller
+public class SocketController {
+
+    @Autowired
+    private WebSocketServer webSocketServer;
+
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/webSocket")
+    public ModelAndView socket() {
+        ModelAndView mav=new ModelAndView("/webSocket");
+//        mav.addObject("userId", userId);
+        return mav;
+    }
+
+
+}
