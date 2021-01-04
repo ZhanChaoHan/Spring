a9c0feae5970bab64236bf676250eec3fc92e040
jachs <799516423@qq.com>
2021-01-04 09:43:39 +0800

    ngfe

---
+++ boot/spring-boot-database/src/test/java/com/jachs/database/BeETest.java
@@ -0,0 +1,36 @@
+package com.jachs.database;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author zhanchaohan
+ * 
+ */
+@SpringBootTest
+public class BeETest {
+    @Autowired
+    @Qualifier("c3p0Source")
+    private DataSource c3ds;
+    
+    @Autowired
+    @Qualifier("dbcpSource")
+    private DataSource dbds;
+    
+    @Autowired
+    @Qualifier("druidDataSource")
+    private DataSource druidDataSource;
+    
+    @Test
+    public void bsTest() throws SQLException {
+        System.out.println ( c3ds.getConnection ().isClosed () );
+        System.out.println ( dbds.getConnection ().isClosed () );
+        System.out.println ( druidDataSource.getConnection ().isClosed () );
+    }
+}
