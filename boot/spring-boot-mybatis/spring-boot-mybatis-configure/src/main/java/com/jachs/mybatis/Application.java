7335c756e5720ceb1d77ca878a4d8b5c7741c5e0
jachs <799516423@qq.com>
2021-03-18 18:05:46 +0800

    'deazc'

--- boot/spring-boot-mybatis/spring-boot-mybatis-configure/src/main/java/com/jachs/mybatis/Application.java
+++ boot/spring-boot-mybatis/spring-boot-mybatis-configure/src/main/java/com/jachs/mybatis/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootMybatisConfigureApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMybatisConfigureApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
