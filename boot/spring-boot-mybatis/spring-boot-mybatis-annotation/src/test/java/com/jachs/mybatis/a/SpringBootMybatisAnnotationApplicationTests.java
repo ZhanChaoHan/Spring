cd6b3929857647450f28db24fc8c0aeeb2259a6d
jachs <799516423@qq.com>
2021-03-18 15:30:42 +0800

    'init'

---
+++ boot/spring-boot-mybatis/spring-boot-mybatis-annotation/src/test/java/com/jachs/mybatis/a/SpringBootMybatisAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jachs.mybatis.a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMybatisAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
