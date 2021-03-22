eabaabc1c30ea45bcc41e20da9da909ef5b6621d
jachs <799516423@qq.com>
2021-03-22 16:41:05 +0800

    hhchvcueq

--- boot/spring-boot-mybatis/spring-boot-mybatis-configure/src/main/java/com/jachs/mybatis/dao/Soft_wareMapper.java
+++ boot/spring-boot-mybatis/spring-boot-mybatis-configure/src/main/java/com/jachs/mybatis/dao/Soft_wareMapper.java
@@ -1,17 +1,15 @@
 package com.jachs.mybatis.dao;
 
+import java.util.List;
+
 import com.jachs.mybatis.entity.Soft_ware;
 
 public interface Soft_wareMapper {
     int deleteByPrimaryKey(String softWareId);
-
     int insert(Soft_ware record);
-
     int insertSelective(Soft_ware record);
-
     Soft_ware selectByPrimaryKey(String softWareId);
-
     int updateByPrimaryKeySelective(Soft_ware record);
-
     int updateByPrimaryKey(Soft_ware record);
+    List<Soft_ware> selectAll();
 }
--- boot/spring-boot-mybatis/spring-boot-mybatis-configure/src/test/java/com/jachs/mybatisc/dao/ComputerMapperTest.java
+++ boot/spring-boot-mybatis/spring-boot-mybatis-configure/src/test/java/com/jachs/mybatisc/dao/ComputerMapperTest.java
@@ -1,10 +1,14 @@
 package com.jachs.mybatisc.dao;
 
+import java.util.List;
+
 import org.apache.commons.lang.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jachs.mybatis.Application;
 import com.jachs.mybatis.dao.ComputerMapper;
 import com.jachs.mybatis.dao.Soft_wareMapper;
@@ -40,5 +44,41 @@ public class ComputerMapperTest {
 			sw.setSoftWareName(rsu.randomAlphanumeric(6));
 			soft_wareMapper.insert(sw);
 		}
+	}
+	//page helper分页
+	@Test
+	public void test3() {
+		System.out.println("第一页-----------------------");
+		PageHelper.startPage(1, 10);
+		List<Soft_ware>  swList=soft_wareMapper.selectAll();
+		for (Soft_ware soft_ware : swList) {
+			System.out.println(soft_ware.getSoftWareId());
+		}
+		System.out.println("第二页-----------------------");
+		PageHelper.startPage(2, 10);
+		List<Soft_ware>  swList1=soft_wareMapper.selectAll();
+		for (Soft_ware soft_ware : swList1) {
+			System.out.println(soft_ware.getSoftWareId());
+		}
+		
+	}
+	//分页排序
+	@Test
+	public void test4() {
+		String orderBy ="cid desc";////按照排序字段 倒序 排序
+		
+		System.out.println("第一页-----------------------");
+		PageHelper.startPage(1, 10,orderBy);
+		List<Soft_ware>  swList=soft_wareMapper.selectAll();
+		for (Soft_ware soft_ware : swList) {
+			System.out.println(soft_ware.getSoftWareId());
+		}
+		System.out.println("第二页-----------------------");
+		PageHelper.startPage(2, 10,orderBy);
+		List<Soft_ware>  swList1=soft_wareMapper.selectAll();
+		for (Soft_ware soft_ware : swList1) {
+			System.out.println(soft_ware.getSoftWareId());
+		}
+		
 	}
 }
