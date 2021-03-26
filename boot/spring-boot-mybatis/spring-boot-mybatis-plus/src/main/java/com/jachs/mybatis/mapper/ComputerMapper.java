921a9e596676674afcac0581be7eb31597572b65
jachs <799516423@qq.com>
2021-03-26 10:10:42 +0800

    'addplus'

--- boot/spring-boot-mybatis/spring-boot-mybatis-plus/src/main/java/com/jachs/mybatis/mapper/ComputerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.jachs.mybatis.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.jachs.mybatis.entity.Computer;
-
-
-public interface ComputerMapper extends BaseMapper<Computer>{
-
-}
