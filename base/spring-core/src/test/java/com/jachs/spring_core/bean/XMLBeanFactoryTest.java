ef1e2f43c0bce494a4bb92c9f4495f4045cb888d
jachs <799516423@qq.com>
2021-08-16 17:14:28 +0800

    'qefgr'

--- base/spring-core/src/test/java/com/jachs/spring_core/bean/XMLBeanFactoryTest.java
+++ base/spring-core/src/test/java/com/jachs/spring_core/bean/XMLBeanFactoryTest.java
@@ -1,6 +1,5 @@
 package com.jachs.spring_core.bean;
 
-
 import org.junit.Test;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
@@ -28,22 +27,30 @@ public class XMLBeanFactoryTest {
 		System.out.println(people.getName());
 		System.out.println(people.getAge());
 		System.out.println(people.getPhoneNum());
-		
+
 		System.out.println("-----------------------------------");
-		Person person=(Person) beanFactory.getBean("jason");
-		
+		Person person = (Person) beanFactory.getBean("jason");
+
 		System.out.println(person.getName());
 		System.out.println(person.getAge());
 		System.out.println(person.getPhoneNum());
-		
+
 		System.out.println("-----------------------------------");
-		
-		People personCk=(People) beanFactory.getBean("jessica");
-		
+
+		People personCk = (People) beanFactory.getBean("jessica");
+
 		System.out.println(personCk.getName());
 		System.out.println(personCk.getAge());
 		System.out.println(personCk.getPhoneNum());
 	}
 
-	
+	@Test
+	public void testConnection() {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+		reader.loadBeanDefinitions(new ClassPathResource("XMLBeanFactory.xml"));
+		
+		MockConnection mc=(MockConnection) beanFactory.getBean("mockConn");
+		
+	}
 }
