fd08b9d784a009c5b4b2c5c68d92b74a7d1244c9
jachs <799516423@qq.com>
2021-09-02 11:20:20 +0800

    'qdq'

---
+++ spring-framework/j-spring-aop/src/test/java/com/jachs/j_spring_aop/my/UserTest.java
@@ -0,0 +1,22 @@
+package com.jachs.j_spring_aop.my;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.jachs.j_spring_aop.controller.UserController;
+
+/***
+ * 
+ * @author zhanchaohan
+ *
+ */
+public class UserTest {
+	@Test
+	public void test1() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("my/User.xml","my/UserAdvice.xml");
+	
+		UserController uc=ctx.getBean("uController",UserController.class);
+		uc.save();
+	}
+}
